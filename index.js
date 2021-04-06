ee685e5e7c1fbc98a8bcb7c58fb3c71ffb7c7329
Jahid Hasan <slniloy500@gmail.com>
2021-04-06 17:33:32 +0600

    Add server

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config()
+
+app.use(bodyParser.json())
+app.use(cors())
+const port = 5000
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lntcp.mongodb.net/ShareMind?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const userPostCollection = client.db("ShareMind").collection("User");
+
+  app.post("/addPost",(req, res) => {
+      const post = req.body
+      userPostCollection.insertOne(post)
+      .then(result => {
+          res.send(result.insertedCount > 0)
+      })
+  })
+  app.get("/post",(req, res) => {
+      userPostCollection.find({}).sort({_id:-1})
+      .toArray((errors,document) => {
+          res.send(document)
+      })
+  })
+
+
+    console.log('database connect success');
+});
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port)
